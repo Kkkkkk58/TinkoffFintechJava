8c76ca6dca14802aa78d9996516fb3fcaf36c78a
Karim Khasan <karimhasan3879@gmail.com>
2023-10-08 12:54:07 +0300

    style: reformat code

--- fintech/src/main/java/ru/kslacker/fintech/dataaccess/api/CityRepository.java
+++ fintech/src/main/java/ru/kslacker/fintech/dataaccess/api/CityRepository.java
@@ -6,7 +6,10 @@ import java.util.UUID;
 
 public interface CityRepository {
     City createCity(City city);
+
     City getById(UUID cityId);
+
     void deleteById(UUID cityId);
+
     boolean exists(UUID cityId);
 }
