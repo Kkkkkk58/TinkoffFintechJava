bd36d24156e96c4f87bf3d604aec176be12da8ed
Karim Khasan <karimhasan3879@gmail.com>
2023-10-29 18:54:07 +0300

    test: add base integration test config

---
+++ fintech/src/test/java/ru/kslacker/fintech/controllers/TestContainersH2Test.java
@@ -0,0 +1,43 @@
+package ru.kslacker.fintech.controllers;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.utility.DockerImageName;
+
+@SpringBootTest
+public abstract class TestContainersH2Test {
+
+    @Container
+    public static GenericContainer h2 = new GenericContainer(DockerImageName.parse("oscarfonts/h2"))
+            .withExposedPorts(1521, 81)
+            .withEnv("H2_OPTIONS", "-ifNotExists")
+            .waitingFor(Wait.defaultWaitStrategy());
+
+    @DynamicPropertySource
+    static void dataSourceProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.driverClassName", () -> "org.h2.Driver");
+        registry.add("spring.datasource.url", () -> "jdbc:h2:tcp://localhost:" + h2.getMappedPort(1521) + "/test");
+        registry.add("spring.datasource.username", () -> "sa");
+    }
+
+    @Test
+    public void contextLoads() {
+    }
+
+    @BeforeEach
+    public void setUp() {
+        h2.start();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        h2.stop();
+    }
+}
